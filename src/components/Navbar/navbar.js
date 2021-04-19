c6f7f69df89113a7b38125026f9eeeb04580974d
LinosM <Zemnas768@gmail.com>
2021-04-19 17:11:12 -0400

    changed src to /

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -4,7 +4,7 @@ import "./navbar.css";
 function Navbar() {
     return (
         <nav className="navbar navbar-light bg-light">
-            <a className="navbar-brand" href="#">
+            <a className="navbar-brand" href="/">
                 Employee Directory
             </a>
         </nav>
