02264b41bfdf5cccfd11be861002c3b372c563ba
LinosM <Zemnas768@gmail.com>
2021-04-19 17:11:36 -0400

    added barebones employee table

--- src/components/Body/body.js
+++ src/components/Body/body.js
@@ -1,1 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
+import API from "../../utils/API";
+import EmployeeTable from "../EmployeeTable/employeetable";
+
+class Body extends Component {
+    state = {
+        result: [],
+        search: ""
+    };
+
+    componentDidMount() {
+        this.searchEmp("");
+    };
+
+    searchEmp = query => {
+        API.search(query)
+            .then(res => {
+                this.setState({ result: res.data.results });
+                console.log(this.state.result);
+            })
+            .catch(err => console.log(err));
+    };
+
+    render() {
+        return (
+            <EmployeeTable 
+                employees={this.state.result}
+            />
+        );
+    };
+}
+
+export default Body;
