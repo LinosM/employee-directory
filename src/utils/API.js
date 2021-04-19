ff40a215b30c01a476ed04fe98d56b17e470c0af
LinosM <Zemnas768@gmail.com>
2021-04-19 17:09:52 -0400

    added 200 search limit, set nationalities to US

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASEURL = "https://randomuser.me/api/";
+const BASEURL = "https://randomuser.me/api/?results=200&nat=us";
 
 // Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
