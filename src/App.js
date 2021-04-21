4d2d88d1c43797006a0bf8095faea22908399783
LinosM <Zemnas768@gmail.com>
2021-04-21 17:55:08 -0400

    added wrapper component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Body from "./components/Body";
+import Wrapper from "./components/Wrapper"
 import "./App.css"
 
 function App() {
   return (
-    <div>
+    <div className="body">
       <Navbar />
-      <div className="container">
-        <Body />
-      </div>
+      <Wrapper>
+        <div className="container">
+          <Body />
+        </div>
+      </Wrapper>
     </div>
   )
 }
